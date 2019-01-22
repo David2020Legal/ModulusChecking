e8582583ab0325fe6a260e0b8b6360f812b36e2e
Daveforrester23 <dmforrester23@googlemail.com>
2019-01-22 08:52:36 +0000

    Implement database into project.

---
+++ Finance.ModulusChecking.SQLServer/dbo/Tables/Weightings.sql
@@ -0,0 +1,23 @@
+﻿CREATE TABLE [dbo].[Weightings] (
+    [ID]               INT          IDENTITY (1, 1) NOT NULL,
+    [SortCodeFrom]     NCHAR (6)    NOT NULL,
+    [SortCodeTo]       NCHAR (6)    NOT NULL,
+    [Method]           NCHAR (5)    NOT NULL,
+    [WeightingDigit1]  INT          NOT NULL,
+    [WeightingDigit2]  INT          NOT NULL,
+    [WeightingDigit3]  INT          NOT NULL,
+    [WeightingDigit4]  INT          NOT NULL,
+    [WeightingDigit5]  INT          NOT NULL,
+    [WeightingDigit6]  INT          NOT NULL,
+    [WeightingDigit7]  INT          NOT NULL,
+    [WeightingDigit8]  INT          NOT NULL,
+    [WeightingDigit9]  INT          NOT NULL,
+    [WeightingDigit10] INT          NOT NULL,
+    [WeightingDigit11] INT          NOT NULL,
+    [WeightingDigit12] INT          NOT NULL,
+    [WeightingDigit13] INT          NOT NULL,
+    [WeightingDigit14] INT          NOT NULL,
+    [Exception]        NVARCHAR (8) NULL,
+    CONSTRAINT [PK_Weightings] PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
---
+++ Finance.ModulusChecking.SQLServer/dbo/Views/WeightingsView.sql
@@ -0,0 +1,42 @@
+﻿CREATE View WeightingsView AS
+SELECT ID As WeightingId, 1 As Digit, WeightingDigit1 As Value
+FROM Weightings
+UNION
+SELECT ID, 2, WeightingDigit2
+FROM Weightings
+UNION
+SELECT ID, 3, WeightingDigit3
+FROM Weightings
+UNION
+SELECT ID, 4, WeightingDigit4
+FROM Weightings
+UNION
+SELECT ID, 5, WeightingDigit5
+FROM Weightings
+UNION
+SELECT ID, 6, WeightingDigit6
+FROM Weightings
+UNION
+SELECT ID, 7, WeightingDigit7
+FROM Weightings
+UNION
+SELECT ID, 8, WeightingDigit8
+FROM Weightings
+UNION
+SELECT ID, 9, WeightingDigit9
+FROM Weightings
+UNION
+SELECT ID, 10, WeightingDigit10
+FROM Weightings
+UNION
+SELECT ID, 11, WeightingDigit11
+FROM Weightings
+UNION
+SELECT ID, 12, WeightingDigit12
+FROM Weightings
+UNION
+SELECT ID, 13, WeightingDigit13
+FROM Weightings
+UNION
+SELECT ID, 14, WeightingDigit14
+FROM Weightings
